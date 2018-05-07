956909f8fb21eec9896ecdeaeeb685f08bef1a49
Mukundh Bhushan <30744831+MukundhBhushan@users.noreply.github.com>
2018-05-07 23:42:10 +0530

    added the blocckchain logic

---
+++ blockchain.py
@@ -0,0 +1,59 @@
+import hashlib as hasher
+import datetime
+import json
+
+
+class Block():
+    def __init__(self, index, time_stamp, data, peri_hash):
+        self.data=data
+        self.index=index
+        self.time_stamp=time_stamp
+        self.peri_hash=peri_hash
+        self.hash=self.make_hash()
+
+    def make_hash(self):
+        sha = hasher.sha256((str(self.index) + str(self.time_stamp) +str(self.data) +str(self.peri_hash)).encode('utf-8'))
+
+        return sha.hexdigest()
+
+
+def genisi_block():
+    blk = Block(0, datetime.datetime.now(), "Genesis Block", "0")
+    return blk
+
+def next_block(blockObj,in_data):
+    index=blockObj.index +1
+    time_stamp=datetime.datetime.now()
+    data=in_data
+    peri_hash=blockObj.hash
+    return(Block(index, time_stamp, data, peri_hash))
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime.datetime, datetime.date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+
+if __name__=="__main__":
+    blockchain = [genisi_block()]
+    #blockchain .append()
+    res=input("yes or no ")
+    previous_block=blockchain[0]
+    blockchain.pop()
+    blockchain.append(json.dumps(previous_block.__dict__,default=json_serial))
+
+    while res=="yes":
+        data=input("enter data ")
+        new_block=next_block(previous_block,data)
+        previous_block=new_block
+        new_block=json.dumps(new_block.__dict__,default=json_serial)
+        blockchain.append(new_block)
+        print(new_block)
+        res = input("add a block")
+
+    if res=="no":
+        for i,j in enumerate(blockchain):
+            print(i,j)
+
